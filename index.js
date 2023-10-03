816f78430e22ee7ff7efcfc2739ba3297b005a3f
zachary <zmh012004@gmail.com>
2023-10-02 22:54:49 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 function addingEventListener() {
 }
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
+function addingEventListener() {
+  const input = document.getElementById('button');
+  input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
+}
+
+addingEventListener();
